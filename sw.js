32f82f56103ed512632b9b8c930003432f328dce
mikeymr6-tech <mikeymr6@aol.com>
2026-08-25 14:17:41 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="aws-performance-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./all-weather-seal-logo.jpeg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
